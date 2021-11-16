495d5a405edfe860b04439bc908a4b560a782b26
Draxis1000 <45176640+Draxis1000@users.noreply.github.com>
2021-11-16 11:59:01 +0800

    Main premise changed, delete function added
    Modified the original code to fit the requirements of the group, added as well is the delete entries function. Changed the database fields to suit the columns names needed. Look for every file for changes.

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ connection = sqlite3.connect("data.db")
 
 def create_table():
     connection.execute(
-        "CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT)")
+        "CREATE TABLE IF NOT EXISTS entries (id INTEGER, name TEXT, dateDelivery TEXT, time TEXT, paymentMode TEXT, contactNum TEXT)")
     connection.commit()
 
 
@@ -12,11 +12,15 @@ def close_database():
     connection.close()
 
 
-def add_entry(entry_content, entry_date):
+def add_entry(entry_id, entry_name, entry_dateDelivery, entry_time, entry_paymentMode, entry_contactNum):
     connection.execute(
-        f"INSERT INTO entries VALUES ('{entry_content}', '{entry_date}');")
+        f"INSERT INTO entries VALUES ({entry_id}, '{entry_name}', '{entry_dateDelivery}', '{entry_time}', '{entry_paymentMode}', '{entry_contactNum}');")
     connection.commit()
 
 
 def get_entries():
     return connection.execute("SELECT * FROM entries;")
+
+def del_entry(id_for_deletion):
+    connection.execute(f"DELETE FROM entries WHERE id={id_for_deletion};")
+    connection.commit()
--- main.py
+++ main.py
@@ -1,36 +1,47 @@
-from database import create_table, add_entry, get_entries, close_database
+from database import create_table, add_entry, get_entries, close_database, del_entry
 
 create_table()
 menu = """
 Please select one of the following option:
 1. Add new entry
 2. View entries
-3. Exit
+3. Delete entries
+4. Exit
 
 Your selection: """
 
 
 def prompt_new_entry():
-    entry_content = input("What have you learned today? ")
-    entry_date = input("Enter date: ")
-    add_entry(entry_content, entry_date)
+    entry_id = input("Enter ID: ")
+    entry_name = input("Enter name of what: ")
+    entry_dateDelivery = input("Enter delivery date: ")
+    entry_time = input("Enter delivery time: ")
+    entry_paymentMode = input("Enter payment mode used: ")
+    entry_contactNum = input("Enter contact number: ")
+    add_entry(entry_id, entry_name, entry_dateDelivery, entry_time, entry_paymentMode, entry_contactNum)
 
 
 def view_entries(entries):
     for entry in entries:
         print(f"-----------------------------")
-        print(f"{entry[1]} - {entry[0]}")
+        print(f"{entry[0]}\n{entry[1]}\n {entry[2]} - {entry[3]}\n {entry[4]}\n {entry[5]}")
+
+def prompt_del_entry():
+    id_for_deletion = input("Enter ID needed to be deleted: ")
+    del_entry(id_for_deletion)
 
 
-welcome = "Welcome to your diary!"
+welcome = "Welcome to the Delivery Record App!"
 print(welcome)
 user_input = input(menu)
 
-while user_input != "3":
+while user_input != "4":
     if user_input == "1":
         prompt_new_entry()
     elif user_input == "2":
         view_entries(get_entries())
+    elif user_input =="3":
+        prompt_del_entry()
     else:
         print("Invalid options, please try again")
 
